e37c5fb35eaa3cdf892b5b8c4c2177d6d842ec23
MaryGz <mriagzh@gmail.com>
2022-12-18 15:16:18 -0600

    Add files via upload

---
+++ day6.1-solution.py
@@ -0,0 +1,20 @@
+packet = []
+index = 0
+PACKET_LENGTH = 4
+with open("day6-input.txt", "r") as f:
+    line = f.readline()
+    s = list(line)
+
+    for i in range(0, len(s)):
+        packet.append(s[i])
+        index += 1
+        if len(packet) == PACKET_LENGTH:
+            c = []
+            for j in range(0, PACKET_LENGTH):
+                x = packet.count(packet[j])
+                c.append(x)
+
+            if sum(c) == PACKET_LENGTH:
+                print(index)
+                break
+            packet.pop(0)
